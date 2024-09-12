666daf4c7b64464e410b82feb66fe21a428d96e8
sofa-byte <privateemailuser22@gmail.com>
2024-09-12 08:03:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -289,4 +289,5 @@ io.on('connection', socket => {
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(\`Server running on port \${PORT}\`);
+    console.log(server running XD);
 });
